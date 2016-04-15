9d8d80cdebec46ad8288fe8440a9e1ba63dc04a4
0xFireball <0xFireball@outlook.com>
2016-04-14 23:58:24 -0700

    add basic

---
+++ EPad/MainForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Eto.Forms;
+using Eto.Drawing;
+using Eto.Serialization.Xaml;
+using System.Reflection;
+using System.IO;
+
+namespace EPad
+{
+    public class MainForm : Form
+    {
+        Splitter splitter1;
+        OmniEditorX.TextEditorX textEditor;
+        WebView webView;
+
+        public MainForm()
+        {
+            this.ClientSize = new Size(640, 480);
+            textEditor = new OmniEditorX.TextEditorX()
+            {
+                Width = this.ClientSize.Width / 2,
+                Height = this.ClientSize.Height,
+            };
+            webView = new WebView();
+            splitter1 = new Splitter()
+            {
+                Width = this.ClientSize.Width,
+                Orientation = Orientation.Horizontal,
+                Panel1 = textEditor,
+                Panel2 = webView,
+            };
+            Content = new StackLayout()
+            {
+                Items =
+                {
+                    splitter1
+                },
+            };
+            Icon = Icon.FromResource("EPad.icon.ico");
+            textEditor.TextChanged += TextEditor_TextChanged;
+        }
+
+        private void TextEditor_TextChanged(object sender, EventArgs e)
+        {
+            webView.LoadHtml(textEditor.Text);
+        }
+
+        protected void HandleQuit(object sender, EventArgs e)
+        {
+            Application.Instance.Quit();
+        }
+    }
+}
--- OmniEditorX/TextEditorX.cs
+++ OmniEditorX/TextEditorX.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Eto.Forms;
 using Eto.Drawing;
+using System.Text.RegularExpressions;
 
 namespace OmniEditorX
 {
@@ -15,14 +16,54 @@ namespace OmniEditorX
             Shown += LayoutEditor;
         }
 
+        string lastText = "";
+
+        int tabLevel = 0;
+        string tokens = "(EPad)";
+
         private void SetUpEditor(object sender, EventArgs e)
         {
             Font = new Font(FontFamilies.Monospace, 10);
+            SelectionForeground = Colors.Black;
         }
 
         private void LayoutEditor(object sender, EventArgs e)
         {
-            this.Size = Parent.ClientSize;
+            
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            if (Text != lastText)
+            {
+                lastText = Text;
+                Regex rX = new Regex(tokens);
+                MatchCollection matches = rX.Matches(Text);
+                int StartCursorPosition = Selection.Start;
+                foreach (Match m in matches)
+                {
+                    int startIndex = m.Index;
+                    int StopIndex = m.Index + m.Length;
+                    Select(startIndex, StopIndex);
+                    SelectionForeground = Colors.Blue;
+                    SelectionBold = true;
+                    Select(StartCursorPosition, StartCursorPosition - 1);
+                    SelectionBold = false;
+                    SelectionForeground = Colors.Black;
+                    Select(StartCursorPosition + 1, StartCursorPosition);
+                }
+            }
+        }
+
+        protected void Select(int startIndex, int stopIndex)
+        {
+            Selection = new Range<int>(startIndex, stopIndex);
         }
     }
 }
