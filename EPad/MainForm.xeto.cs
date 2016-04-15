b5124314bc7e80aa4f2ccbb831f9176b4502525f
0xFireball <0xFireball@outlook.com>
2016-04-14 22:54:04 -0700

    basic stuff

---
+++ EPad/MainForm.xeto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Eto.Forms;
+using Eto.Drawing;
+using Eto.Serialization.Xaml;
+using System.Reflection;
+using System.IO;
+
+namespace EPad
+{
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            XamlReader.Load(this);
+            Content = new StackLayout()
+            {
+                Items =
+                {
+                    new OmniEditorX.TextEditorX()
+                },
+            };
+            Icon = Icon.FromResource("EPad.icon.ico");
+        }
+
+        protected void HandleQuit(object sender, EventArgs e)
+        {
+            Application.Instance.Quit();
+        }
+    }
+}
---
+++ OmniEditorX/TextEditorX.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace OmniEditorX
+{
+    /// <summary>
+    /// A custom panel
+    /// </summary>
+    public class TextEditorX : RichTextArea
+    {
+        public TextEditorX()
+        {
+            PreLoad += SetUpEditor;
+            Shown += LayoutEditor;
+        }
+
+        private void SetUpEditor(object sender, EventArgs e)
+        {
+            Font = new Font(FontFamilies.Monospace, 10);
+        }
+
+        private void LayoutEditor(object sender, EventArgs e)
+        {
+            this.Size = Parent.ClientSize;
+        }
+    }
+}
